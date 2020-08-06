9f9571f0a02aab790080c5bc618a3707b5ffc0cf
VitorG718 <vitorx1280@gmail.com>
2020-08-06 15:41:41 -0300

    Adicionada um base de dados no arquivo do servidor para teste da template engine

--- src/server.js
+++ src/server.js
@@ -1,3 +1,29 @@
+// Simulando a base de dados
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "61995953315",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
+        whatsapp: "11993174695",
+        bio: "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar.<br><br>Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: \"Aprenda a fazer dinheiro com isso!\"",
+        subject: "Educação Física",
+        cost: "40",
+        weekday: [5],
+        time_from: [1500],
+        time_to: [2020]
+    }
+];
+
 const express = require("express"); // objeto do express
 const server = express(); // funcionalidades do express
 
